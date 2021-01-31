863bf5cbc98bef76de6d64701dc9cc7120422c60
Tadas <tadasgailevicius@yahoo.com>
2021-02-01 00:09:24 +0200

    Add Queries to InvoiceApp.Application/Invoices

---
+++ InvoiceApp.Application/Invoices/Queries/GetUserInvoicesQuery.cs
@@ -0,0 +1,13 @@
+﻿using InvoiceApp.Application.Invoices.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceApp.Application.Invoices.Queries
+{
+    public class GetUserInvoicesQuery: IRequest<IList<InvoiceVm>>
+    {
+        public string User { get; set; }
+    }
+}
