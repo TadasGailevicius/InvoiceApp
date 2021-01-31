7cb2885783cf8e860c2f4ef9b82bda00c235e926
Tadas <tadasgailevicius@yahoo.com>
2021-02-01 00:11:49 +0200

    Add Validators to InvoiceApp.Application/Invoices

---
+++ InvoiceApp.Application/Invoices/Validators/MustHaveInvoiceItemProperty.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Validators;
+using InvoiceApp.Application.Invoices.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InvoiceApp.Application.Invoices.Validators
+{
+    public class MustHaveInvoiceItemPropertyValidator : PropertyValidator
+    {
+        public MustHaveInvoiceItemPropertyValidator()
+            : base("Property {PropertyName} should not be an empty list.")
+        {
+        }
+
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            var list = context.PropertyValue as IList<InvoiceItemVm>;
+            return list != null && list.Any();
+        }
+    }
+}
