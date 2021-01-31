554c7a907cb9df7b3afbb4841c147eb3cc35cf9d
Tadas <tadasgailevicius@yahoo.com>
2021-02-01 00:15:40 +0200

    Add DependencyInjection.cs to InvoiceApp.Application

---
+++ InvoiceApp.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using InvoiceManagementApp.Application.Common.Behaviors;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace InvoiceApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            return services;
+        }
+
+    }
+}
