ab1d7920a30f33216a64991cffd1067dec06cb45
Tadas <tadasgailevicius@yahoo.com>
2021-02-01 21:38:48 +0200

    Add InvoiceMappingProfile.cs to InvoiceApp.Application\MappingProfiles

---
+++ InvoiceApp.Application/MappingProfiles/InvoiceMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using InvoiceApp.Application.Invoices.Commands;
+using InvoiceApp.Application.Invoices.ViewModels;
+using InvoiceApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvoiceApp.Application.MappingProfiles
+{
+    public class InvoiceMappingProfile : Profile
+    {
+        public InvoiceMappingProfile()
+        {
+            CreateMap<Invoice, InvoiceVm>();
+            CreateMap<InvoiceItem, InvoiceItemVm>().ConstructUsing(i => new InvoiceItemVm
+            {
+                Id = i.Id,
+                Item = i.Item,
+                Quantity = i.Quantity,
+                Rate = i.Rate
+            });
+
+            CreateMap<InvoiceVm, Invoice>();
+            CreateMap<InvoiceItemVm, InvoiceItem>();
+
+            CreateMap<CreateInvoiceCommand, Invoice>();
+        }
+    }
+}
