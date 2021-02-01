8c98ce409e4c870892a504602c97b75f5b16d0fd
Tadas <tadasgailevicius@yahoo.com>
2021-02-01 21:40:59 +0200

    Add AutoMapper to DependencyInjection in InvoiceApp.Application

--- InvoiceApp.Application/DependencyInjection.cs
+++ InvoiceApp.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
+using FluentValidation;
 using InvoiceManagementApp.Application.Common.Behaviors;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,15 +10,15 @@ using System.Text;
 
 namespace InvoiceApp.Application
 {
-    public static class DependencyInjection
+    public static class DependecyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
             return services;
         }
-
     }
 }
